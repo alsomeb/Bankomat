715c7243308132100f140ef7369ec0b6515ec5c5
alsomeb <alsomeb@gmail.com>
2021-09-20 15:14:57 +0200

    almost final

--- bankomat_ny.py
+++ bankomat_ny.py
@@ -1,4 +1,14 @@
-accounts = {} # dictionary    #TERMCOLOR? för snygg GUI
+import json
+import datetime
+import time
+accounts = {} #konton, finns ett 123 test konto i log.json, den måste finnas där annars körs ej programmet
+log_list = {} #loggar transaktioner, samma som ovan
+
+with open("log.json") as json_file: #öppnar fil för kontonr och tillhörande saldo
+    accounts = json.load(json_file)
+
+with open("trans.json") as json_file: #öppnar fil för transaktioner
+    log_list = json.load(json_file)
 
 def MainMenu():
     print("****HUVUDMENY****")
@@ -11,7 +21,8 @@ def AccountMenu():
     print("1. Ta ut pengar")
     print("2. Sätt in pengar")
     print("3. Visa saldo")
-    print("4. Avsluta")
+    print("4. Lista transaktioner")
+    print("5. Avsluta")
 
 while True:
     anykey = input("Tryck enter för att fortsätta: ")
@@ -26,11 +37,12 @@ while True:
             kontonummer = input("Ange ett nytt kontonr: ")
             if kontonummer in accounts:
                 print("Kontonummret finns redan!")
-                tryAgain = input("Vill du prova ett annat kontonummer, (j/n): ").lower()
+                tryAgain = input("Vill du prova ett annat kontonummer, (j/n): ").lower() #om kontonr nr redan finns = felmed
                 if tryAgain != "j":
                     break
             else:
                 accounts[kontonummer] = 0  # Skapar ett nytt kontonummer i accounts med 0 saldo i dictionaryn (accounts[key] = value)
+                log_list[kontonummer] = []
                 break
     if val == "2":
         kontonummer = input("Ange kontonummer: ")
@@ -40,27 +52,50 @@ while True:
                 AccountMenu()
                 val = input("Ange menyval: ")
                 
-                if val == "4": #Avsluta
+                if val == "5": #Avsluta
                     break
                 if val == "1": #Ta ut pengar
-                    uttag = int(input("Ange belopp: "))
-                    saldo = accounts[kontonummer]
-                    if uttag > saldo:
-                        print(f"Du har inte tillräckligt på kontot, ditt saldo är {saldo}")
+                    uttag = input("Ange belopp: ")
+                    if uttag.isnumeric() == True:
+                        uttag = int(uttag) #gör om inputen till en int
+                        saldo = accounts[kontonummer]
+                        if uttag > saldo:
+                            print(f"Du har inte tillräckligt på kontot, ditt saldo är {saldo}")
+                        else:
+                            accounts[kontonummer] = saldo - uttag
+                            print(f"Du har gjort ett uttag på: {uttag}")
+                            log_list[kontonummer].append(f"{datetime.date.today()}, {time.strftime('%H:%M')}, UTTAG: {uttag}kr")
                     else:
-                        accounts[kontonummer] = saldo - uttag
-                        print(f"Du har gjort ett uttag på: {uttag}")
+                        print("Endast siffror tillåtna, ej bokstäver och specialtecken!") #kodat så att man ej kan skriva +,- eller andra bokstäver
 
                 if val == "3": #Visa saldo
                     saldo = accounts[kontonummer]
                     print(f"Ditt saldo är: {saldo}")
 
                 if val == "2": #Sätt in pengar
-                    insattning = int(input("Ange belopp: "))
-                    saldo = accounts[kontonummer]
-                    accounts[kontonummer] = saldo + insattning
-                    print(f"Du har gjort en insattning på: {insattning}kr på ditt konto")
+                    insattning = input("Ange belopp: ")
+                    if insattning.isnumeric() == True:
+                        insattning = int(insattning)
+                        saldo = accounts[kontonummer]
+                        accounts[kontonummer] = saldo + insattning
+                        print(f"Du har gjort en insattning på: {insattning}kr på ditt konto")
+                        log_list[kontonummer].append(f"{datetime.date.today()}, {time.strftime('%H:%M')}, INSÄTTNING: {insattning}kr")
+                    else:
+                        print("Endast siffror tillåtna, ej bokstäver och specialtecken!") #kodat så att man ej kan skriva +,- eller andra bokstäver
+                    
+                if val == "4": #Loopar igenom trans.json filen och printar transaktionshistoriken för aktuellt konto
+                    for trans in log_list[kontonummer]:
+                        print(f"{trans}\n")
                
             else:
                 print("Du har angivit fel kontonr, går tillbaka till huvudmeny")
                 break
+
+    j = json.dumps(accounts)
+    with open("log.json", "w") as f: #sparar kontonr och tillhörande saldo
+        f.write(j)
+    
+    t = json.dumps(log_list)
+    with open("trans.json", "w") as f: #spara transaktioner
+        f.write(t)
+
