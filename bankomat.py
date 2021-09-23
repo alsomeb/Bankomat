e11cf6c696fe6225b8eae7226de94c94f8c0d407
alsomeb <alsomeb@gmail.com>
2021-09-23 12:22:43 +0200

    Final

--- bankomat.py
+++ bankomat.py
@@ -1,94 +1,95 @@
 import json
 import datetime
 import time
-accounts = {} #konton, finns ett 123 test konto i log.json, den måste finnas där annars körs ej programmet
-log_list = {} #loggar transaktioner i trans.json, samma som ovan
+from functions import MainMenu
+from functions import AccountMenu
+from termcolor import colored,cprint #färger används för snygg "GUI"
+accounts = {} #konton
+log_list = {} #loggar transaktioner i trans.json
 
 with open("log.json") as json_file: #öppnar fil för kontonr och tillhörande saldo
     accounts = json.load(json_file)
 
+
 with open("trans.json") as json_file: #öppnar fil för transaktioner
-    log_list = json.load(json_file)
+     log_list = json.load(json_file)
 
-def MainMenu():
-    print("****HUVUDMENY****")
-    print("1. Skapa konto")
-    print("2. Administrera konto")
-    print("3. Avsluta")
 
-def AccountMenu():
-    print("****KONTOMENY*****")
-    print("1. Ta ut pengar")
-    print("2. Sätt in pengar")
-    print("3. Visa saldo")
-    print("4. Lista transaktioner")
-    print("5. Avsluta")
-
+#start, såg mer fördelar köra inputsen som string, om man skrev fel så loopas menyn bara om, förutom belopp körs med INT
 while True:
-    anykey = input("Tryck enter för att fortsätta: ")
+    anykey_text = colored("Tryck enter för att fortsätta: ", color="green", attrs=["bold"])#färg och atr för anykey
+    anykey = input(anykey_text)#gör så att det blir snyggare med outputen, men kan bli tjatigt kanske
     MainMenu()
-    val = input("Ange menyval: ")
+    val_text = colored("Ange menyval: ", color="green", attrs=["bold"]) #val styling
+    val = input(val_text)
 
     if val == "3": #Avslutar bankomaten
         break
-
     if val == "1":
         while True:
-            kontonummer = input("Ange ett nytt kontonr: ")
+            kontonummer = input(colored("Ange ett nytt kontonr: ", 'green', attrs=['bold'])) #annan metod för styling
             if kontonummer in accounts:
-                print("Kontonummret finns redan!")
-                tryAgain = input("Vill du prova ett annat kontonummer, (j/n): ").lower() #om kontonr nr redan finns = felmed
+                cprint("Kontonummret finns redan!", 'red', attrs=['bold'])
+                tryAgain = input(colored("Vill du prova ett annat kontonummer, (j/n): ", 'green', attrs=['bold']).lower()) #om kontonr nr redan finns = felmed
                 if tryAgain != "j":
                     break
             else:
+                cprint(f"Du har skapat ett konto med kontonr: {kontonummer}", 'green', attrs=['bold'])
                 accounts[kontonummer] = 0  # Skapar ett nytt kontonummer i accounts med 0 saldo i dictionaryn (accounts[key] = value)
-                log_list[kontonummer] = [] # lägger in en tom lista i dict value (nesting). Tex kontonr "123" = []
+                log_list[kontonummer] = [] # lägger in en tom lista i dict value (nesting). Tex kontonr "123" = [tom lista]
                 break
+    if val != "3" and val != "2" and val != "1": #felmed om ej valt rätt menyval
+        print(colored("Du måste välja en siffra mellan 1-3", 'red', attrs=['bold']))
     if val == "2":
-        kontonummer = input("Ange kontonummer: ")
+        kontonummer = input(colored("Ange kontonummer: ", 'green', attrs=['bold']))
         while True:
             if kontonummer in accounts:
-                anykey = input("Tryck enter för att fortsätta: ")
+                anykey_text = colored("Tryck enter för att fortsätta: ", color="green", attrs=["bold"])
+                anykey = input(anykey_text)
                 AccountMenu()
-                val = input("Ange menyval: ")
+                val_text2 = colored("Ange menyval: ", color="green", attrs=["bold"]) #val styling
+                val = input(val_text2)
                 
                 if val == "5": #Avsluta
                     break
                 if val == "1": #Ta ut pengar
-                    uttag = input("Ange belopp: ")
-                    if uttag.isnumeric() == True:
+                    uttag = input(colored("Ange belopp: ", 'green', attrs=['bold']))
+                    if uttag.isnumeric(): #kollar att det är siffror
                         uttag = int(uttag) #gör om inputen till en int
                         saldo = accounts[kontonummer]
                         if uttag > saldo:
-                            print(f"Du har inte tillräckligt på kontot, ditt saldo är {saldo}")
+                            print(colored(f"Du har inte tillräckligt på kontot, ditt saldo är {saldo}", 'red', attrs=['bold']))
                         else:
                             accounts[kontonummer] = saldo - uttag
-                            print(f"Du har gjort ett uttag på: {uttag}")
-                            log_list[kontonummer].append(f"{datetime.date.today()}, {time.strftime('%H:%M')}, UTTAG: {uttag}kr")
+                            print(colored(f"Du har gjort ett uttag på: {uttag}", 'green', attrs=['bold']))
+                            log_list[kontonummer].append(colored(f"{datetime.date.today()}, {time.strftime('%H:%M')}, UTTAG: {uttag}kr", 'red', attrs=['bold']))
                     else:
-                        print("Endast siffror tillåtna, ej bokstäver och specialtecken!") #kodat så att man ej kan skriva +,- eller andra bokstäver
+                        cprint("Endast siffror tillåtna, ej bokstäver och specialtecken!", 'red', attrs=['bold']) #kodat så att man ej kan skriva +,- eller andra bokstäver
 
                 if val == "3": #Visa saldo
                     saldo = accounts[kontonummer]
-                    print(f"Ditt saldo är: {saldo}")
+                    print(colored(f"Ditt saldo är: {saldo}", 'green', attrs=['bold']))
 
                 if val == "2": #Sätt in pengar
-                    insattning = input("Ange belopp: ")
-                    if insattning.isnumeric() == True:
+                    insattning = input(colored("Ange belopp: ", 'green', attrs=['bold']))
+                    if insattning.isnumeric(): #kollar att det är siffror
                         insattning = int(insattning)
                         saldo = accounts[kontonummer]
                         accounts[kontonummer] = saldo + insattning
-                        print(f"Du har gjort en insattning på: {insattning}kr på ditt konto")
-                        log_list[kontonummer].append(f"{datetime.date.today()}, {time.strftime('%H:%M')}, INSÄTTNING: {insattning}kr")
+                        print(colored(f"Du har gjort en insattning på: {insattning}kr på ditt konto", 'green', attrs=['bold']))
+                        log_list[kontonummer].append(colored(f"{datetime.date.today()}, {time.strftime('%H:%M')}, INSÄTTNING: {insattning}kr", 'green', attrs=['bold']))
                     else:
-                        print("Endast siffror tillåtna, ej bokstäver och specialtecken!") #kodat så att man ej kan skriva +,- eller andra bokstäver
+                        cprint("Endast siffror tillåtna, ej bokstäver och specialtecken!", 'red', attrs=['bold']) #kodat så att man ej kan skriva +,- eller andra bokstäver
                     
                 if val == "4": #Loopar igenom trans.json filen och printar transaktionshistoriken för aktuellt konto
                     for trans in log_list[kontonummer]:
                         print(f"{trans}\n")
+
+                if val != "1" and val != "2" and val != "3" and val != "4" and val != "5": #felmed om ej valt rätt menyval
+                    print(colored("Du måste välja en siffra mellan 1-5", 'red', attrs=['bold']))
                
             else:
-                print("Du har angivit fel kontonr, går tillbaka till huvudmeny")
+                cprint("Du har angivit fel kontonr, går tillbaka till huvudmeny", 'red', attrs=['bold'])
                 break
 
     #döpte variablerna bara till random bokstäver, har inge större betydelse, bara komma ihåg att skriva dem rätt
---
+++ functions.py
@@ -0,0 +1,20 @@
+from termcolor import colored,cprint
+#Funktioner som används i bankomaten, tänkte lägga till mer men blev termcolor istället
+
+def MainMenu(): #Med termcolor
+    menu = colored("****HUVUDMENY****", color="green", attrs=["reverse", "bold"])
+    print(menu)
+    print("1. Skapa konto")
+    print("2. Administrera konto")
+    print("3. Avsluta")
+    return #så att den inte skapar flera instancer av sig själv
+
+def AccountMenu():
+    acc_menu = colored("****KONTOMENY****", color="green", attrs=["reverse", "bold"])
+    print(acc_menu)
+    print("1. Ta ut pengar")
+    print("2. Sätt in pengar")
+    print("3. Visa saldo")
+    print("4. Lista transaktioner")
+    print("5. Avsluta")
+    return #stäng funkt, return inget
